8906134e3e15c68ccc8bc38b5829c75665b7f67b
jcrippen <jcrippen@gmail.com>
2016-01-13 14:36:47 -0800

    Emacs default settings for editing corpus files.
    These are some default settings for Emacs when editing corpus files.
    Emacs’s whitespace-mode is enabled to make tabs marked distinctively,
    making it easier to see when spaces are used wrongly instead of tabs.

---
+++ .dir-locals.el
@@ -0,0 +1,14 @@
+;;; This file contains configuration stuff for editing corpus files in Emacs.
+((text-mode . ((whitespace-style . (face tabs trailing empty
+                                    space-before-tab space-after-tab
+                                    tab-mark))
+               (whitespace-display-mappings . ((space-mark 32 [183] [46])
+                                               (space-mark 160 [164] [95])
+                                               (newline-mark 10 [9166 10]
+                                                             [182 10]
+                                                             [36 10])
+                                               (tab-mark 9 [8680 9]
+                                                         [8677 9]
+                                                         [187 9]
+                                                         [92 9])))
+               (eval . (whitespace-mode)))))
