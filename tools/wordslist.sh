310f3f8034c7095d811797e5119e4328091ad31f
James A. Crippen <jcrippen@gmail.com>
2020-06-09 13:58:13 -0700

    Clone from wordcount.sh
    Simple tool that lists all unique words in the corpus.

---
+++ tools/wordslist.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+#
+# wordcount.sh
+#
+# Count the number of words in all files given on the command line. This
+# strips line numbers and all metadata in the input files before counting.
+#
+# Arguments: <filename1> [<filename2> ...]
+
+# "$@" produces "$1" "$2" "$3" etc. We need this because our filenames
+# contain spaces and so must be quoted.
+cat "$@" | \
+	# Strip initial line numbers and tabs. Sed can’t handle \t apparently
+	# so we use a raw tab character.
+	sed -E 's/^[0-9]*	(.*)/\1/' | \
+	# Delete metadata lines, those enclosed in {...}.
+	sed -E '/^\{.*\}$/d' | \
+	# Break lines into words.
+	tr -s '[[:punct:][:space:]]' '\n' | \
+	# Sort words
+	sort | \
+	# Filter unique words.
+	uniq
